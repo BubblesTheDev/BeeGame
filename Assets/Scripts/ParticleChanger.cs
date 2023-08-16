88445d06e3541bbe97942267e736a63a4673fe32
cptbilliau <cptbilliau@gmail.com>
2023-08-16 19:21:20 -0400

    added more particle behaviour

---
+++ Assets/Scripts/ParticleChanger.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem _particleSystem;
+    
+    private GameObject Target;
+    private Transform targetTransform;
+    
+   
+
+    void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        Target = GameObject.FindGameObjectWithTag("Player");
+        targetTransform = Target.transform;
+    }
+
+
+    void LateUpdate()
+    {
+        if (_particleSystem.isPlaying)
+        {
+            
+            var emission = _particleSystem.emission;
+            var main = _particleSystem.main;
+            
+
+            if (Vector3.Distance(targetTransform.position, transform.position) <= 10)
+            {
+                
+                emission.rateOverTime = 7.5f;
+                main.startSizeMultiplier = 1.4f;
+            }
+            else 
+            {
+
+                emission.rateOverTime = 5;
+                main.startSizeMultiplier = 1;
+            }
+            
+        }
+    }
+}
