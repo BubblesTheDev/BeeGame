a25eb826e6ffa491309d4b8008d0ef5e9f3907d6
cptbilliau <cptbilliau@gmail.com>
2023-08-24 16:36:36 -0400

    updated fmod, improved music in transition scene

--- Assets/Scripts/DAYTExtScript.cs
+++ Assets/Scripts/DAYTExtScript.cs
@@ -13,10 +13,10 @@ public class DAYTExtScript : MonoBehaviour
 
     //FIRST DAY IS DAY 1, NOT DAY 0 
     public int DayNum;
-    EventInstance daystart;
+    
     void Start()
     {
-        daystart = RuntimeManager.CreateInstance("event:/Mus_startofday");
+        
     }
 
     void Update()
@@ -25,20 +25,12 @@ public class DAYTExtScript : MonoBehaviour
         if (tl.time >= 3f)
         {
             anim.SetInteger("DayNum", DayNum);
-            if (PlaybackState(daystart) != PLAYBACK_STATE.PLAYING)
-            {
-                daystart.start();
-            }
+            
 
         }
 
 
 
     }
-    FMOD.Studio.PLAYBACK_STATE PlaybackState(FMOD.Studio.EventInstance instance)
-    {
-        FMOD.Studio.PLAYBACK_STATE pS;
-        instance.getPlaybackState(out pS);
-        return pS;
-    }
+    
 }
