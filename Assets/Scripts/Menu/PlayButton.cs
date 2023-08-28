9eb1e778d2fc70ba920e741ae7f2be3763b7d6f3
cptbilliau <cptbilliau@gmail.com>
2023-08-28 16:46:07 -0400

    fixed music in main menu

--- Assets/Scripts/Menu/PlayButton.cs
+++ Assets/Scripts/Menu/PlayButton.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using FMODUnity;
-
+using FMOD.Studio;
 
 public class PlayButton : MonoBehaviour
 {   
     public string sceneName;
+    EventInstance MainMenuMusic;
 
+    private void Awake()
+    {
+        MainMenuMusic = RuntimeManager.CreateInstance("event:/MainMenuMusic");
+        MainMenuMusic.start();
+    }
     public void PlayButtonStart()
     {
         StartCoroutine(PlayStartSound());
+        
     }
 
     IEnumerator PlayStartSound()
     {
+        //MainMenuMusic.setPaused(true);
+        MainMenuMusic.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         RuntimeManager.PlayOneShot("event:/GameStart");
         yield return new WaitForSeconds(1.6f);
         Debug.Log("changinScenes");
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        //MainMenuMusic.setPaused(false);
     }
 }
