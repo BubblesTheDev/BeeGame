6ad1c6c44848ef9ec99f5f4f0757d8fa5ab4a4cd
GrayAidan <114709817+GrayAidan@users.noreply.github.com>
2023-08-31 11:29:33 -0400

    stuff

--- Assets/Scripts/Menu/MenuBeeManager.cs
+++ Assets/Scripts/Menu/MenuBeeManager.cs
@@ -14,6 +14,8 @@ public class MenuBeeManager : MonoBehaviour
 
     public List<GameObject> beeSpawnInScene;
 
+    private bool secret;
+
     private void Start()
     {
         BeeSpawn();
@@ -22,11 +24,29 @@ public class MenuBeeManager : MonoBehaviour
         Invoke("BeeSpawn", Random.Range(0f, 2f));
         Invoke("BeeSpawn", Random.Range(0f, 2f));
         Invoke("BeeSpawn", 1);
+
+        secret = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey("y") && Input.GetKey("u") && Input.GetKey("i"))
+        {
+            secret = true;
+        }
     }
 
     public void BeeSpawn()
     {
         GameObject newBee = Instantiate(beePrefab, spawnPoints[Random.Range(0, spawnPoints.Count)].transform.position, transform.rotation);
+
+        if (secret)
+        {
+            float randomSize = Random.Range(0.1f, 7f);
+
+            newBee.transform.localScale *= randomSize;
+            newBee.transform.GetChild(1).transform.localScale *= randomSize;
+        }
 
         BeeHavior _bh = newBee.GetComponent<BeeHavior>();
         _bh.exitPoint = exitPoints[Random.Range(0, exitPoints.Count)];
